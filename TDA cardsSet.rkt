2fd1896a7f51a0c57aa2f3f427108b4b7d532d56
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-04-01 21:10:09 -0300

    Definición de TDA cardsSet

---
+++ TDA cardsSet.rkt
@@ -0,0 +1,35 @@
+#lang racket
+; TDA Game
+
+(define cardsSet
+  (lambda (Elements numE maxC rndFn)
+    (+ 1 n)))
+
+(define dobble?
+  (lambda (cardsSet-entrada)
+    (1)))
+
+(define numCards
+  (lambda (cardsSet-entrada)
+    ))
+
+(define nthCard
+  (lambda (cardsSet-entrada carta-seleccionada)
+    (carta)))
+
+(define findTotalCards
+  (lambda (carta-entrada)
+    ))
+
+(define requiredElements
+  (lambda (carta-entrada)
+    ))
+
+(define missingCards
+  (lambda (cardsSet-entrada)
+    (cardsSet-salida)))
+
+(define cardsSet->string
+  (lambda (cardsSet-entrada)
+    (cardsSet-string)))
+
