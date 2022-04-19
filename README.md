2643d96b3d98d8c194c330df2c04dd3e47513a9f
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-04-18 23:52:33 -0400

    Orden de algoritmo a presentar

--- README.md
+++
@@ -1,2 +0,0 @@
-# PP-L1-22
-Repositorio que almacena el código requerido para el laboratorio 1 de paradigmas de programación
--- TDA game.rkt
+++ TDA game.rkt
@@ -1,31 +1,9 @@
 #lang racket
 
-(define mi-filtro
- (lambda (pred lista)
-   (cond
-     [(null? lista) null]
-     [(pred(car lista)) ;; Si predicado retorna True, ejecuto la siguiente línea. Caso contrario, ejecutar else
-      (cons (car lista) (mi-filtro pred (cdr lista)))]
-     [else (mi-filtro pred (cdr lista))])))
+; TDA game
+; Representación computacional de la ejecución del juego
+; con sus respectivas reglas, elementos necesarios para la experiencia de juego
+; y respetando las restricciones
+; correspondientes del juego Dobble.
 
-(define pertenece?
-  (lambda (x lst)
-    (cond
-      [(null? lst) #f]
-      [(eq? x (car lst)) #t]
-      [else
-       (pertenece? x (cdr lst))])))
-
-(map eq? '(1 4 3) '(1 2 3))
-
-(define elemento-comun?
-   (lambda (lista)
-     (cond
-       [(null? lista) #f]
-       ; Condición [... #t]
-       [else
-        (elemento-comun? (cdr lista))])))
-
-(define dobble-par?
-   (lambda (carta1 carta2)
-  (map eq? carta1 carta2)))
+; Representación: Listas
