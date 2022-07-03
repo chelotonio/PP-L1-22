17ccb7845d0025a02f40ad6237b7793de296f8ca
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-07-03 17:53:10 -0400

    Creación de funcion dobble?

--- TDA cardsSet.rkt
+++ TDA cardsSet.rkt
@@ -16,6 +16,8 @@
 (define baraja
   null)
 
+; ______________ TDA CardsSet - Constructor ______________
+
 ; Función que agrega una carta a a una baraja determinada.
 ; Dominio: baraja-entrada (list) X  carta (list)
 ; Recorrido: list of lists
@@ -76,7 +78,7 @@
           (else (cons (buscar-elemento 1 (+ (* (- numE 1) j) (+ k 0)) Elements-entrada) (n-cartas-2 (+ k 1) j numE Elements-entrada))))))
 
 ; Función que almacena las primeras n (numE - 1) cartas de la baraja.
-; Dominio: j (int) X numE (int) X Elments-entrada (list)
+; Dominio: j (int) X numE (int) X Elements-entrada (list)
 ; Recorrido: list of lists
 (define n-cartas
       (lambda (j numE Elements-entrada)
@@ -104,31 +106,67 @@
       (else baraja-final))
     ))
 
+; ______________ TDA CardsSet - dobble? ______________
 
-  
-; (dobble? baraja
-; Verificar que los elementos en una carta sean distintos,
-; y que para cada carta existe un solo elemento en común
-; para cualquier otra carta de la baraja.
-; Crear función que revise toda la lista (recursión)
+; Función que cambia valores booleanos por valores enteros (#t -> 1, #f -> 0).
+(define cambiar-boolean
+  (lambda (lista)
+    (cond
+      ((null? lista) null)
+      ((car lista) (cons 1 (cambiar-boolean (cdr lista))))
+      (else (cons 0 (cambiar-boolean (cdr lista)))))))
 
-(define dobble-par?
+; Función que compara un par de cartas y comprueba que solo haya un elemento en común.
+(define es-par-dobble?
   (lambda (carta1 carta2)
-    (map eq? carta1 carta2)))
+    (cond
+      ((eq? 1 (contar-1s (cambiar-boolean (map eq? carta1 carta2)) 0)))
+      (else #f))))
 
+; Función que cuenta la cantidad de elementos en común de un par de cartas.
+(define contar-1s
+  (lambda (lista i)
+    (cond
+      ((null? lista) i)
+      ((eq? (car lista) 1) (contar-1s (cdr lista) (+ i 1)))
+      (else (contar-1s (cdr lista) i)))))
+
+; Función que cuenta la cantidad de cartas presentes en una baraja.
+(define contar-baraja
+  (lambda (lista i)
+    (cond
+      ((null? lista) i)
+      (else (contar-baraja (cdr lista) (+ i 1))))))
+
+; Función que devuelve falso si quedan más de 1 carta en la baraja, en caso contrario devuelve verdadero.
+(define no-queda-par-de-cartas?
+  (lambda (baraja)
+    (cond
+      ((< 1 (contar-baraja baraja 0)) #f)
+      (else #t))))
+
+;
+;
+;
+(define dobble?
+  (lambda (cardsSet-entrada)
+    (cond
+      ((no-queda-par-de-cartas? cardsSet-entrada) #t)
+      ((es-par-dobble? (car cardsSet-entrada) (car (cdr cardsSet-entrada))) (dobble? (cdr cardsSet-entrada)))
+      (else #f))))
 
 ; Función que recorre todas las cartas revisando que cada carta tenga elementos distintos.
-; Dominio: 
+; Dominio: cards (list)
 ; Salida: boolean
 ;(define diff-elements?
 
 ; Función que...
-; Entrada:
-; Salida:
+; Entrada: cardsSet (List)
+; Salida: boolean
 ;(define dobble?
 ;  (lambda (cardsSet-entrada)
 ;    (cond
-;      ((((dobble-par? cardsSet) and (diff-elements? cardsSet))) #t)
+;      ((((dobble-par? cardsSet-entrada) and (diff-elements? cardsSet-entrada))) #t)
 ;      (else #f))))
 
 ; Función que...
@@ -173,4 +211,13 @@
   (lambda (cardsSet-entrada)
     (null)))
 
-;(cardsSet '(1 2 3 4 5 6 7 8 9 10 11 12 13) 4 10 0)
+; Ejemplos de CardsSet.
+; (cardsSet '(1 2 3 4 5 6 7 8 9 10 11 12 13) 4 10 0)
+; (cardsSet '("Arból" "Manzana" "Plátano" "Zorro" "Lana" "Cama" "Silla") 3 10 0)
+; (cardsSet '("A" "B" "C" "D" "E" "F" "G") 2 10 0)
+
+; Ejemplos de dobble?.
+; (dobble? (cardsSet '(1 2 3 4 5 6) 2 10 0))
+; (dobble? '((1 2 3) (1 4 5) (1 6 7) (2 6 8) (2 7 9) (3 4 9)))
+; (dobble? '((1 2) (1 3) (1 3))) ... Ejemplo donde coinciden dos cartas con más de un elemento en común.
+; (dobble? '((1 2) (2 4) (5 6))) ... Ejemplo donde ninguna carta posee un elemento en común.
